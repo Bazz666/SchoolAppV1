89c1abf4d87de6e947c9e0832a3a4b82f25e0eec
bazz666 <herrera.se1992@gmail.com>
2021-10-03 11:06:42 -0300

    Start:first commmit, start new projet with progresql

---
+++ db/migrate/20211003140140_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :lastName
+      t.date :birthDate
+      t.integer :adress
+      t.integer :role
+
+      t.timestamps
+    end
+  end
+end
