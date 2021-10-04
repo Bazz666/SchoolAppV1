67af26bf12cd6218ccbb59f276680b4f3780c3c2
bazz666 <herrera.se1992@gmail.com>
2021-10-04 15:20:40 -0300

    feat: install faker gem an n' programing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# Assesment.destroy_all
+# Course.destroy_all
+# Departament.destroy_all
+# Message.destroy_all
+# Room.destroy_all
+# User.destroy.all
+
+
+
+30.times do
+    User.create!(
+        name: Faker::Name.first_name,
+        lastName: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: password, 
+        password_confirmation: password
+    ) 
+end
+
+10.times do
+    Assesment.create!(
+        grade: rand(1..10)
+    ) 
+end
+
+10.times do
+    Room.create!(
+        name: Faker::Name.first_name
+    ) 
+end
+
+10.times do
+    Departament.create!(
+        name: Faker::Educator.subject 
+    ) 
+end
+
+10.times do
+    Course.create!(
+        Faker::Educator.course_name
+    ) 
+end
