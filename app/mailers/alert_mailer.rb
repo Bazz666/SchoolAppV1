33f9b1d793cdb239ee57f57917eeb46ee42c124c
bazz666 <herrera.se1992@gmail.com>
2021-11-01 14:22:13 -0300

    fix:heroku mailing

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -2,6 +2,6 @@ class AlertMailer < ActionMailer::Base
 
     def alert_user(user)
         @user = User.all
-        mail to: user.email, subject: "Bienvenido", from: "schoolapp.mailer@gmail.com", body: "Haz sido agregado a un nuevo curso!"
+        mail to: User.course.all, subject: "Bienvenido", from: "schoolapp.mailer@gmail.com", body: "Haz sido agregado a un nuevo curso!"
     end 
 end
