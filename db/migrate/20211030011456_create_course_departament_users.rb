067819b1c433a46f19dc5c0684b2c3e09771aa44
bazz666 <herrera.se1992@gmail.com>
2021-11-01 13:52:09 -0300

    fix: push heroku

--- db/migrate/20211030011456_create_course_departament_users.rb
+++ db/migrate/20211030011456_create_course_departament_users.rb
@@ -1,6 +1,6 @@
-class CreateCoursedepartamentsrtamentUsers < ActiveRecord::Migration[5.2]
+class CreateCourseDepartamentsUsers < ActiveRecord::Migration[5.2]
   def change
-    create_table :course_departamentsrtament_users do |t|
+    create_table :course_departament_users do |t|
       t.references :course, foreign_key: true
       t.references :departamentsrtament, foreign_key: true
       t.references :user, foreign_key: true
